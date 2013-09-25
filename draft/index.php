6b41226e3424927a3d54212a93700116cbec6a80
glancom <gallus.glanzmann@gmail.com>
2013-09-25 10:45:56 +0200

    added Text to remove  #4

--- draft/index.php
+++ draft/index.php
@@ -6,10 +6,13 @@
  *	Website Index
  */
 
-try {
-	require_once('./includes/configuration/main.php');
+ 
+ // Text to remove
+ 
+try {require_once('./includes/configuration/main.php');
 
 	require(PATH_VIEW.'design.php');
+	
 }
 catch (PDOException $e) {
 	require_once(PATH_VIEW.'database_error.php');
